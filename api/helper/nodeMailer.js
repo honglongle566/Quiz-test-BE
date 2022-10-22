52be20a0468b2be75b16d62a25f8de918113031b
damvanthuc <vanthuc@eplaza.vn>
2022-10-22 10:50:06 +0700

    add nodeMailer

--- api/helper/nodeMailer.js
+++ api/helper/nodeMailer.js
@@ -3,11 +3,11 @@ let transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     type: 'OAuth2',
-    user: ``,
-    pass: ``,
-    clientId: ``,
-    clientSecret: ``,
-    refreshToken: ``
+    user: `lehonglong307@gmail.com`,
+    pass: `LEHONGLONG566`,
+    clientId: `832646059385-j3nhimreusra534imkkg2a2hi2tmjamq.apps.googleusercontent.com`,
+    clientSecret: `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`,
+    refreshToken: `1//04Ofu39WXpMIDCgYIARAAGAQSNwF-L9IrNyWN8hkzxT7Bnhv4hqUCYC22146S-3StR4bgSt0FXRfOOhh7mhwySnoC-A2S6OY6H_0`
   }
 });
 transporter.verify(function (error, success) {
